72ea901966daf5351a9e74d64623a02fa7456fbf
ArthurMousatov <arthurmousatov@gmail.com>
2021-09-16 19:17:51 -0400

    Uploaded Past Work

---
+++ plant_humidity_sensor/src.ino
@@ -0,0 +1,41 @@
+/*
+
+*/
+#include "Arduino_MCHPTouch.h"
+#include <Arduino_MKRIoTCarrier.h>
+
+int moistPin = A5;
+MKRIoTCarrier carrier;
+int minMoisture = 0;
+int maxMoisture = 100;
+int moistureThreshold = 50;
+bool
+
+void setup() {
+  Serial.begin(9600);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500);
+  
+  CARRIER_CASE = false;
+  carrier.begin();
+}
+
+void loop() {
+  int rawMoistValue = analogRead(moistPin);
+  int moistValue = rawMoistValue;
+  
+  if(moistValue > moistureThreshold)
+    displayGoodHealth();
+  else
+    displayBadHealth(moistValue);
+  
+  delay(1000);
+}
+
+void displayGoodHealth(int moistureValue){
+  Serial.println("I'm in good health! Moisture: " + moistureValue);
+}
+
+void displayBadHealth(int moistureValue){
+  Serial.println("I'm in bad health! Moisture: " + moistureValue);
+}
