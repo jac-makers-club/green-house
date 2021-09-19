b1983cb28d788b61733776507914c21096b1bb10
Sarim <1783364@johnabbottcollege.net>
2021-09-19 16:54:09 -0400

    LCD Changes Depending On MoistValue. Extra Variables Added For Potential Use

--- plant_humidity_sensor/src/src.ino
+++ plant_humidity_sensor/src/src.ino
@@ -6,9 +6,15 @@
 
 int moistPin = A5;
 MKRIoTCarrier carrier;
-int minMoisture = 700;
-int maxMoisture = 1023;
-int moistureThreshold = 50;
+//Deviding the range in 4 parts so that the user has less guesswork to do
+//These extra variables will most likely be unused and deleted but I just wanted to be sure in case we wanted to implement more ranges
+int minMoisture = 670; //Completely wet
+int oneThirdsMoisture = 758.25; //good moisture
+int twoThirdsMoisture = 846.5; //Wet enough
+int threeThirdMOisture = 934.75; //Somewhat wet
+int maxMoisture = 1023; //Completely Dry
+//Was 50
+int moistureThreshold = 848;
 
 void setup() {
   Serial.begin(9600);
@@ -23,21 +29,24 @@ void loop() {
   int rawMoistValue = analogRead(moistPin);
   int moistValue = rawMoistValue;
   
-  if(moistValue > moistureThreshold)
+  if(moistValue < moistureThreshold){
+   //The moisture level is reaching wettest level (Could be problematic as too much water is bad)
     displayGoodHealth(moistValue);
+  }
   else
     displayBadHealth(moistValue);
   
   delay(1000);
 }
 
-void displayGoodHealth(int moistureValue){
-  Serial.print("I'm in good health! Moisture: ");
-  Serial.println(moistureValue);
-  //The LCD display
-  carrier.display.fillScreen(ST77XX_ORANGE);
-  carrier.display.setTextColor(ST77XX_WHITE);
 
+void displayGoodHealth(int moistureValue){
+  
+  //The LCD display
+  carrier.display.fillScreen(ST77XX_GREEN);
+  carrier.display.setTextColor(ST77XX_WHITE);
+Serial.print("I'm in good health! Moisture: ");
+  Serial.println(moistureValue);
   carrier.display.setCursor(30, 50);
   carrier.display.print("Moisture Level: ");
   carrier.display.setTextSize(4);
