f0af7ef1565bf071721efd805a145ef5b12defed
Sarim <78218025+SarimSyed@users.noreply.github.com>
2021-09-26 09:35:46 -0400

    Plant humidity sensor (#2)
    * Uploaded Past Work
    
    * Stray Bool Removed
    
    * LCD Now Displays Moisture Level And Color Of The Screen Changes Color Based On Moisture Level.
    
    * LCD Changes Depending On MoistValue. Extra Variables Added For Potential Use
    
    * remove old file
    
    * add config files for VS Code Arduino plugin
    
    * Prevent Screen flicker by displaying new moist values only
    
    * add vsconfig to gitignore & remove from repo
    
    Co-authored-by: ArthurMousatov <arthurmousatov@gmail.com>
    Co-authored-by: Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
    Co-authored-by: ArthurMousatov <57199800+ArthurMousatov@users.noreply.github.com>

---
+++ plant_humidity_sensor/src/src.ino
@@ -0,0 +1,74 @@
+/*
+
+*/
+#include "Arduino_MCHPTouch.h"
+#include <Arduino_MKRIoTCarrier.h>
+
+int moistPin = A5;
+MKRIoTCarrier carrier;
+//Deviding the range in 4 parts so that the user has less guesswork to do
+//These extra variables will most likely be unused and deleted but I just wanted to be sure in case we wanted to implement more ranges
+int minMoisture = 670; //Completely wet
+int oneThirdsMoisture = 758.25; //good moisture
+int twoThirdsMoisture = 846.5; //Wet enough
+int threeThirdMOisture = 934.75; //Somewhat wet
+int maxMoisture = 1023; //Completely Dry
+//Was 50
+int moistureThreshold = 848;
+int lastMoisture = -1;
+
+void setup() {
+  Serial.begin(9600);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  delay(1500);
+  
+  CARRIER_CASE = false;
+  carrier.begin();
+}
+
+void loop() {
+  int rawMoistValue = analogRead(moistPin);
+  int moistValue = rawMoistValue;
+  
+  if(lastMoisture != moistValue){
+    //The moisture level is reaching wettest level (Could be problematic as too much water is bad)
+    if(moistValue < moistureThreshold)
+      displayGoodHealth(moistValue);
+    else
+      displayBadHealth(moistValue);
+  }
+
+  lastMoisture = moistValue;
+  delay(1000);
+}
+
+
+void displayGoodHealth(int moistureValue){
+  
+  //The LCD display
+  carrier.display.fillScreen(ST77XX_GREEN);
+  carrier.display.setTextColor(ST77XX_WHITE);
+  Serial.print("I'm in good health! Moisture: ");
+  Serial.println(moistureValue);
+  carrier.display.setCursor(30, 50);
+  carrier.display.setTextSize(4);
+  carrier.display.print("Moisture Level: ");
+  carrier.display.setCursor(40, 120);
+  carrier.display.print(moistureValue);
+  
+  
+}
+
+void displayBadHealth(int moistureValue){
+  Serial.print("I'm in bad health! Moisture: ");
+  Serial.println(moistureValue);
+
+  carrier.display.fillScreen(ST77XX_RED);
+  carrier.display.setTextColor(ST77XX_WHITE);
+
+  carrier.display.setCursor(30, 50);
+  carrier.display.setTextSize(4);
+  carrier.display.print("Moisture Level: ");
+  carrier.display.setCursor(40, 120);
+  carrier.display.print(moistureValue);
+}
