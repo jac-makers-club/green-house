0a1397953457f33314174018302571f970f5e211
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2021-10-23 12:11:45 -0400

    single relayToogle func w/ pass by reference

--- relay-tester/relay-tester.ino
+++ relay-tester/relay-tester.ino
@@ -1,98 +1,66 @@
 #include <Arduino_MKRIoTCarrier.h>
 MKRIoTCarrier carrier;
 
-bool relay_1 = false;
-bool relay_2 = false;
-String relayState1 = "OFF";
-String relayState2 = "OFF";
-
 void setup()
 {
-    // Initialize serial and wait for port to open:
     Serial.begin(115200);
-    // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
-
     carrier.begin();
-    delay(200);
-    onUpdateDisplayChange();
+
+    // Make the touch buttons less sensitive (from 0 to 100). Trial and error.
+    carrier.Buttons.updateConfig(50);
+    updateDisplay();
 }
 
 void loop()
 {
+    // get the latest state of the buttons
     carrier.Buttons.update();
 
     if (carrier.Button3.getTouch())
     {
         Serial.println("Button 3 Touched");
-        onUpdateDisplayChange();
+        updateDisplay();
     }
-
     if (carrier.Button1.getTouch())
     {
         Serial.println("Button 1 Touched");
-        // Toogle the value of relay_1
-        toogleRelay1();
-        onUpdateDisplayChange();
+        toogleRelay(carrier.Relay1);
+        updateDisplay();
     }
-
     if (carrier.Button2.getTouch())
     {
         Serial.println("Button 2 Touched");
-        // Toogle the value of relay_1
-        toogleRelay2();
-        onUpdateDisplayChange();
+        toogleRelay(carrier.Relay2);
+        updateDisplay();
     }
 
-    delay(200);
+    delay(200); // to de-bounce the touch sensors
 }
 
-void toogleRelay1()
-{    
-    if (relay_1)
-    {
-        relay_1 = false;
-        relayState1 = "OFF";
-        carrier.Relay1.open();
-        Serial.println("relay 1 is OFF");
-    }
-    else
-    {
-        relay_1 = true;
-        relayState1 = "ON";
-        carrier.Relay1.close();
-        Serial.println("relay 1 is ONs");
-    }
-}
-
-void toogleRelay2()
-{    
-    if (relay_2)
-    {
-        relay_2 = false;
-        relayState2 = "OFF";
-        carrier.Relay2.open();
-        Serial.println("relay 2 is OFF");
-    }
-    else
-    {
-        relay_2 = true;
-        relayState2 = "ON";
-        carrier.Relay2.close();
-        Serial.println("relay 2 is ON");
-    }
-}
-
-void onUpdateDisplayChange()
+// Use & to access the relay with 'pass by reference' 
+void toogleRelay(MKRIoTCarrier_Relay &relay)
 {
-  carrier.display.fillScreen(ST77XX_WHITE);
-  carrier.display.setTextColor(ST77XX_RED);
-  carrier.display.setTextSize(2);
+    if (relay.getStatus() == HIGH) // Normally Open position
+    {
+        relay.close();
+        Serial.println("Relay " + String(relay.getPin()) + " is ON");
+    }
+    else { // Normally Closed position
+        relay.open(); 
+        Serial.println("Relay " + String(relay.getPin()) + "is OFF");
+    }
+}
 
-  carrier.display.setTextColor(ST77XX_BLUE);
-  carrier.display.setCursor(20, 120);
-  carrier.display.print("R1: ");
-  carrier.display.print(relayState1);
+void updateDisplay()
+{
+    carrier.display.fillScreen(ST77XX_WHITE);
+    carrier.display.setTextColor(ST77XX_RED);
+    carrier.display.setTextSize(3);
+    carrier.display.setTextColor(ST77XX_BLUE);
+    carrier.display.setCursor(20, 120);
 
-  carrier.display.print(" R2: ");
-  carrier.display.print(relayState2);
+    carrier.display.print("R1: ");
+    carrier.display.print(carrier.Relay1.getStatus());
+    carrier.display.print(" R2: ");
+    carrier.display.print(carrier.Relay2.getStatus());
 }
