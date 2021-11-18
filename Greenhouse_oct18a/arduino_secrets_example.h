e6127d3b59fa96d61e0a16cd3c2d58b0fec77901
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2021-11-18 11:31:02 -0500

    updated thingsProperties.h according to cloud code

---
+++ Greenhouse_oct18a/arduino_secrets_example.h
@@ -0,0 +1,10 @@
+// Example of arduino_secrets.h file.
+// The real arduino_secrets.h file is the .gitignore list so it's not pushed to github
+// To use this file:
+// 1. Create a copy and remove "_example" from filename
+// 2. Add your wifi SSID and Password
+// 3. Add your Arduino Cloud "Thing" ID
+
+#define SECRET_SSID "Your wifi name"
+#define SECRET_PASS "Very secret password"
+#define SECRET_ID "12345-thing-id-abcdefg"
